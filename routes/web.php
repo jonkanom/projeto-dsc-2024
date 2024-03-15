acd8d673a9573418ca96a44b22a17f617205f303
JONATHAN MORGADO XAVIER <jonathan.xavier@academico.ifrn.edu.br>
2024-03-15 20:32:24 -0300

    criação de controller e migration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+/*
 Route::get('/novo-arquivo', function () {
     return view('teste');
-});
+});*/
+
+Route::get('/novo-arquivo',[AlunoController::class,'index']);
