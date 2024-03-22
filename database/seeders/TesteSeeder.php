9b5ff658a09ee127206b5055c5165c3180d3d7da
JONATHAN MORGADO XAVIER <jonathan.xavier@academico.ifrn.edu.br>
2024-03-21 21:55:40 -0300

    dia 21/3

---
+++ database/seeders/TesteSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Teste;
+
+class TesteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Teste::create([
+            'nome'=> "Jonathan Xavier",
+            'cpf' => '05555555555',
+            'peso' => 50,
+            'idade' => 20,
+            'data_nascimento' => '2002-04-03',
+        ]);
+    }
+}
